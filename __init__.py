eac3879d3c16a373febd8283ffde21499a9fcac0
deborah <fabbydebby@gmail.com>
2024-06-12 12:34:30 +0300

    movies table

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
 from lib.model.movies import Movies
 
 
-
-
 Movies.drop_table()
 Movies.create_table()
+
 
 movies1 = Movies.create ("Blindspot", "Investigative and crime", "2015-2020", "7.3/10")
 
@@ -21,16 +20,17 @@ movies7 = Movies.create("Lethal Weapon", "Comedy-drama", "2016-2019", "9.0/10")
 movies8 = Movies.create("Princess switch", "Comedy", "2018", "6.1/10")
 
 # movies8.delete()
-all_movies = Movies.fetch_all()
 
-for movie in all_movies:
-    print(movie)
-print(Movies.all)
+# # all_movies = Movies.fetch_all()
 
-movies1.save()
-movies2.save()
-movies3.save()
-movies4.save()
-movies5.save()
-movies6.save()
-movies7.save() 
+# for movie in all_movies:
+#     print(movie)
+# print(Movies.all)
+
+# movies1.save()
+# movies2.save()
+# movies3.save()
+# movies4.save()
+# movies5.save()
+# movies6.save()
+# movies7.save() 
